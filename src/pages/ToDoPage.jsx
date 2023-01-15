10d23798cb35ba0ad89b8dac0b798ddbca1d9244
Damian-Drzewiecki <drzewiecki.damian96@gmail.com>
2023-01-15 16:11:13 +0100

    Add filter butons and filter functions

--- src/pages/ToDoPage.jsx
+++ src/pages/ToDoPage.jsx
@@ -1,29 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import TaskInput from "../components/TaskInput";
 import TaskList from "../components/TaskList";
-let id = 0;
+
+const FILTER_TYPE = { ALL: "all", COMPLETED: "Completed", UNCOMPLETED: "Uncompleted" }
 
 export default function ToDo() {
+    const [id, setId] = useState(0)
+    const [filteredTasks, setFilteredTasks] = useState([])
     const [tasks, setTasks] = useState([])
+    const [filterState, setFilterState] = useState(FILTER_TYPE.ALL)
 
+    useEffect(() => {
+        if (filterState === FILTER_TYPE.UNCOMPLETED) {
+            setFilteredTasks(tasks.filter((element) => element.isChecked === false))
+        }
+        if (filterState === FILTER_TYPE.COMPLETED) {
+            setFilteredTasks(tasks.filter((element) => element.isChecked === true))
+        }
+        if (filterState === FILTER_TYPE.ALL) {
+            setFilteredTasks(tasks)
+        }
+    }, [filterState, tasks])
 
-    const addNewTask = (userInputText) => {
-        setTasks((prevTasks) => [...prevTasks, { id: id++, text: userInputText, isChecked: false }]
-        )
-    }
+    const addNewTask = useCallback((userInputText) => {
+        setTasks((prevTasks) => [...prevTasks, { id: id, text: userInputText, isChecked: false }])
+        setId((prevId) => prevId + 1)
+    }, [id])
 
-    const removeTask = (idToRemove) => {
+    const removeTask = useCallback((idToRemove) => {
         setTasks((prevTasks) => prevTasks.filter((element) => element.id !== idToRemove))
-    }
+    }, [])
 
-    const checkTask = (idToCheck) => {
+    const checkTask = useCallback((idToCheck) => {
         setTasks((prevTasks) => prevTasks.map((element) => element.id === idToCheck ? { ...element, isChecked: !element.isChecked } : element))
-    }
+    }, [])
 
     return (
         <div>
+            <div>
+                <button type="button" onClick={() => { setFilterState(FILTER_TYPE.ALL) }}>All</button>
+                <button type="button" onClick={() => { setFilterState(FILTER_TYPE.COMPLETED) }}>Completed</button>
+                <button type="button" onClick={() => { setFilterState(FILTER_TYPE.UNCOMPLETED) }}>Uncompleted</button>
+            </div>
             <TaskInput addNewTask={addNewTask} />
-            <TaskList taskList={tasks} removeTask={removeTask} checkTask={checkTask} />
+            <TaskList taskList={filteredTasks} removeTask={removeTask} checkTask={checkTask} />
         </div>
     )
 }
