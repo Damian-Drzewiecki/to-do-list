60a8002312a8acaa0ab9f813b644301147c3f1ee
Damian-Drzewiecki <drzewiecki.damian96@gmail.com>
2023-01-19 15:59:02 +0100

    Add list placeholder, and scrollbar

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,16 +3,23 @@ import "./TaskList.css"
 export default function TaskList(props) {
 
     const { taskList, removeTask, checkTask } = props
+    const onCheck = (elementId, event) => {
+        checkTask(elementId)
+    }
 
     return (
         <div className="container">
             {taskList.map((element) => (
-                <div key={element.id} className={classNames('taskBox', { taskBoxLine: element.isChecked })}>
-                    <input className="taskCheckbox" type="checkbox" checked={element.isChecked} onChange={(event) => { checkTask(element.id) }} />
+                <div key={element.id} className={classNames('taskBox', { taskBoxLine: element.isChecked })} onClick={(event) => { onCheck(element.id, event) }}>
+                    <input className="taskCheckbox" type="checkbox" checked={element.isChecked} readOnly />
                     {element.text}
                     <button className="taskButton" type="button" onClick={() => { removeTask(element.id) }}></button>
                 </div>
             ))}
+            {taskList.length === 0 &&
+                <div className="jobsDone">
+                    No tasks yet
+                </div>}
         </div>
     )
 }
