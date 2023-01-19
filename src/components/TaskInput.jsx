ada3ef303f056a99b4f8d2bbb0cca33dd10d6efb
Damian-Drzewiecki <drzewiecki.damian96@gmail.com>
2023-01-19 15:57:35 +0100

    Allow adding new tasks on enter key

--- src/components/TaskInput.jsx
+++ src/components/TaskInput.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import "./TaskInput.css"
 
 export default function TaskInput(props) {
@@ -7,12 +7,22 @@ export default function TaskInput(props) {
     const onClick = () => {
         addNewTask(inputValue)
         setInputValue("")
-
+    }
+    const enterSubmit = (event, eventValue) => {
+        if (event === "Enter" && eventValue !== "") onClick();
     }
 
     return (
         <div className="taskInputBox">
-            <input className="taskInput" type="text" value={inputValue} placeholder="Type your task..." onChange={(event) => setInputValue(event.target.value)}></input>
+            <input
+                className="taskInput"
+                type="text"
+                value={inputValue}
+                placeholder="Type your task..."
+                onKeyDown={(event) => enterSubmit(event.key, event.target.value)}
+                onChange={(event) => setInputValue(event.target.value)}>
+            </input>
+
             <button className="buttonAddTask" disabled={inputValue === ""} onClick={onClick}>+</button>
         </div >
     )
